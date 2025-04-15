e8c7d13073900a0418baf53f1b035b62c0c4e44e
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2025-04-15 21:52:26 +0500

    fix/ sponsor page fixed with apos

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -62,7 +62,7 @@ export default function SponsorsPage() {
           <h2 className="text-base font-semibold leading-7 text-tedRed">Sponsors & Partners</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Supporters</p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            We're grateful to the organizations that make TEDx Ajou Tashkent possible. Their support enables us to bring
+            We&apos;re grateful to the organizations that make TEDx Ajou Tashkent possible. Their support enables us to bring
             ideas worth spreading to our community.
           </p>
         </div>
